21131e4cec488c102c825c42620628065ce7fe16
chuxian <chulingxian@gmail.com>
2022-07-03 17:33:09 +0800

    update

---
+++ Assets/Target.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Entry : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TargetLichun()
+    {
+        Debug.Log("========================TargetLichun");
+        SceneManager.LoadScene("LichunScene", LoadSceneMode.Single);
+    }
+
+}
