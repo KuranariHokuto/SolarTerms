d473ed4ced716e4b03f441f405aeafbfb75a9c9b
chuxian <chulingxian@gmail.com>
2022-07-13 23:05:56 +0800

    update

--- Assets/Eat.cs
+++ Assets/Eat.cs
@@ -6,27 +6,28 @@ using UnityEngine.UI;
 public class Eat : MonoBehaviour
 {
     public static int count = 0;
-    public GameObject endPrompt;
+    public GameObject endPromptCanvas;
+    public GameObject it;
     // Start is called before the first frame update
     void Start()
     {
-        endPrompt.SetActive(false);
+        endPromptCanvas.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) && RectTransformUtility.RectangleContainsScreenPoint(GetComponent<RectTransform>(), Input.mousePosition)) {
+        if (Input.GetMouseButtonDown(0)) {
             Change();
         }
     }
 
     void Change()
     {
-        gameObject.SetActive(false);
+        it.SetActive(false);
         count++;
-        if (count == 5) {
-            endPrompt.SetActive(true);
+        if (count >= 1) {
+            endPromptCanvas.SetActive(true);
         }
     }
 }
